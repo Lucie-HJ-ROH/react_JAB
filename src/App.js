9718dab0d24b615f0ee58d4976a11e34304cba03
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-11-24 10:39:29 -0500

    Clock Element

--- src/App.js
+++ src/App.js
@@ -5,28 +5,24 @@ import Hello from "./Hello";
 function App() {
     const [color, setColor] = React.useState(JSON.parse(sessionStorage.getItem('color')) || "blue");
     const [count, setCount] = React.useState(JSON.parse(localStorage.getItem('count')) || 0);
-    const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+
     console.log(color);
     console.log(count);
 
     React.useEffect(() => {
         sessionStorage.setItem('color', JSON.stringify(color));
         localStorage.setItem('count', JSON.stringify(count));
-        const interval = setInterval(() =>
-            setTime(new Date().toLocaleTimeString(), 1000));
-        return () => {
-            clearInterval(interval);
-        }
-    }, [color, count, time]);
+
+    }, [color, count]);
     return (<div>
             <DisplayMessage color={color}/>
-            <Clock time = {time}/>
+            <Clock/>
             <p>{count}</p>
             <button onClick={() => {
                 setColor(toggle(color));
                 setCount(count + 1)
             }}>
-                Click me React Main1
+                Click me React
             </button>
         </div>
     );
@@ -48,7 +44,17 @@ function DisplayMessage(props) {
 
 
 function Clock(props) {
-    return <p>React Clock: {props.time} </p>
+    const [time, setTime] = React.useState(
+        new Date().toLocaleTimeString());
+    React.useEffect(() => {
+        const interval = setInterval(() =>
+            setTime(new Date().toLocaleTimeString(), 1000));
+        return () => {
+            clearInterval(interval);
+        }
+    }, [time]);
+
+    return <p>React Clock: {time} </p>
 }
 
 export default App;
