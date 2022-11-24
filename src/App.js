b95281a884ba183b3643d776e69daaf63a972548
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-11-24 09:41:32 -0500

    first lecture of React....

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import Hello from "./Hello";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [color, setColor] = React.useState("blue");
+    const [count, setCount] = React.useState(0);
+    console.log(color);
+    console.log(count);
+    return (<div>
+            <DisplayMessage color={color}/>
+            <Clock time={new Date().toLocaleTimeString()}/>
+            <p>{count}</p>
+            <button onClick={() => {
+                setColor(toggle(color));
+                setCount(count + 1)
+            }}>
+                Click me React Main1
+            </button>
+        </div>
+    );
+}
+function toggle(color) {
+    if (color === "blue") {
+        return "red";
+    } else if (color === "red") {
+        return "green";
+    } else {
+        return "blue";
+    }
 }
 
+function DisplayMessage(props) {
+    return <h1 style={{color: props.color}}>Hello React World</h1>;
+}
+
+
+function Clock(props) {
+    return <p>React Clock: {props.time} </p>
+}
 export default App;
